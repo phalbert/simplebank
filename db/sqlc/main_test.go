543a4ae590881192839da40a7f32512de5212e27
Albert Luganga <ahosea15@gmail.com>
2020-10-22 01:52:23 +0300

    added testing and test for CreateAccount

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var (
+	dbDriver = "postgres"
+	dbSource = os.Getenv("DATABASE_URL")
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/phalbert/simplebank
 
 go 1.15
+
+require (
+	github.com/lib/pq v1.8.0
+	github.com/stretchr/testify v1.6.1
+)
